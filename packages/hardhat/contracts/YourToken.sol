dcec25ec726fb12754c5416e5309b8ae9d04a2fd
yves-ganza <lebonganza@outlook.com>
2022-03-17 15:25:59 -0400

    Create an ERC20 Token

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
-    constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+    constructor() ERC20("Love", "LV") {
+        _mint(0xa7341724c1d8371808E1f084Ec39b0ab51BB6ABf, 1000 * 10 ** 18);
     }
 }
